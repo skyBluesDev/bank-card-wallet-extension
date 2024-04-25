326658854be1f0bb2142b38e78452f6c472da3d0
Mahdi Zarei <mahdizarei0614@gmail.com>
2024-04-25 20:37:56 +0330

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'bank-card-wallet-extension-angular',
+  webDir: 'www'
+};
+
+export default config;
